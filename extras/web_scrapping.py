db763c71b34a1a4e30b329b9fd21c463aa28e9b0
Romulo Martins da Silva <romulo.martins@olxbr.com>
2020-05-14 01:50:35 -0300

    Downloading statements in correct directories.

--- extras/web_scrapping.py
+++ extras/web_scrapping.py
@@ -12,20 +12,25 @@ import os
 
 BASE_URL = 'https://olimpiada.ic.unicamp.br'
 
-def _remove_duplicates(array, param):
+def _remove_all(array, param):
     while param in array: 
         array.remove(param)
     return array
 
 def _extract_name(url):
     elems = url.split('/')
-    elems = _remove_duplicates(elems, '')
+    elems = _remove_all(elems, '')
     return elems.pop()
 
-def _save_file(content, filename):
-    dirname = 'data'
+def _extract_dirname(url, filename = ''):
+    url = url.replace(BASE_URL, '')
+    url = url.replace(filename, '')
+    names = _remove_all(url.split("/"), '')
+    return "/".join(names)
+
+def _save_file(content, filename, dirname = 'data'):
     if not os.path.exists(dirname):
-        os.mkdir(dirname)
+        os.system(f'mkdir -p {dirname}')
     file = open(f'{dirname}/{filename}.md', 'w')
     file.write(content)
     file.close()
@@ -44,23 +49,25 @@ def _extract_statement(html_content):
         return None
     return _remove_form(statement[0])
 
-def extract_statement(url):
+def download_statement(url):
     req = requests.get(url)
-    name = _extract_name(url)
+    filename = _extract_name(url)
+    dirname = _extract_dirname(url)
     if req.status_code == 200:
         statement = _extract_statement(req.content)
         markdown = tomd.convert(str(statement))
-        _save_file(markdown, name)
-        print(f'Successful download of {name}!')
+        _save_file(markdown, filename, dirname)
+        print(f'Successful download of {filename}!')
     else:
-        print(f'We cannot get the request of {name}!')
+        print(f'We cannot get the request of {filename}!')
     
-def extract_main_links(url):
+def exec_scraping(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
     links = soup.select('a[href*="/pratique/p"]')
     links = [l.get('href') for l in links]
     for link in links:
-        extract_statement(f'{BASE_URL}{link}')
+        # print(f'{BASE_URL}{link}')
+        download_statement(f'{BASE_URL}{link}')
 
-extract_main_links('https://olimpiada.ic.unicamp.br/pratique/pj/')
+exec_scraping('https://olimpiada.ic.unicamp.br/pratique/p1/')
